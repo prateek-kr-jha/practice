c2f94a691e7002b6ba57efcf3b2d6ba191b0351c
Prateek <prateekj1211@gmail.com>
2025-01-30 00:45:39 +0530

    formatted code

--- fullstackOpen/part1/courseinfo/src/App.jsx
+++ fullstackOpen/part1/courseinfo/src/App.jsx
@@ -1,26 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const History = (props) => {
-  if(props.allClicks.length == 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
+  if (props.allClicks.length == 0) {
+    return <div>the app is used by pressing the buttons</div>;
   }
 
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
   const [leftClick, setLeftClick] = useState(0);
@@ -29,27 +17,27 @@ const App = () => {
   const [total, setTotal] = useState(0);
 
   const handleLeftClick = () => {
-    setAllClicks(allClicks.concat('L'))
+    setAllClicks(allClicks.concat("L"));
     const updatedLeft = leftClick + 1;
     setLeftClick(updatedLeft);
     setTotal(updatedLeft + rightClick);
-  }
+  };
 
   const handleRightClick = () => {
-    setAllClicks(allClicks.concat('R'));
+    setAllClicks(allClicks.concat("R"));
     const updatedRight = rightClick + 1;
     setRightClick(updatedRight);
     setTotal(leftClick + updatedRight);
-  }
+  };
 
   return (
     <div>
       {leftClick}
-      <Button onClick={handleLeftClick} text={'left'}/>
-      <Button onClick={handleRightClick} text={'right'}/>
+      <Button onClick={handleLeftClick} text={"left"} />
+      <Button onClick={handleRightClick} text={"right"} />
       {rightClick}
-      <History allClicks={allClicks}/>
+      <History allClicks={allClicks} />
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
