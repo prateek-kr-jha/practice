16347acd3cf253158dd70f0279f9115dc523de66
Prateek <prateekj1211@gmail.com>
2025-02-03 01:30:19 +0530

    added json wen server and api call with azios

--- fullstackOpen/part2/notes/src/App.jsx
+++ fullstackOpen/part2/notes/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import Note from './components/Note'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Note from './components/Note';
 
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState(
     'a new note....'
   );
   const [showAll, setShowAll] = useState(false);
+
+  const hook = () => {
+    console.log('effect');
+    axios
+    .get('http://localhost:3001/notes')
+    .then(resp => {
+        console.log('promise fullfilled');
+        setNotes(resp.data);
+    })
+  }
+
+  useEffect(hook, []);
+  console.log('render', notes.length, 'notes');
 
   const noteToShow = showAll 
     ? notes 
