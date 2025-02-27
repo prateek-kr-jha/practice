2ae4f0ba0cd30388b94bfc3d796f127d09428d24
Prateek <prateekj1211@gmail.com>
2025-02-28 00:06:46 +0530

    Added api testing

--- fullstackOpen/part3/noteapp/models/note.js
+++ fullstackOpen/part3/noteapp/models/note.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const config = require('../utils/config'); 
 
 mongoose.set('strictQuery', false);
 
-const url = process.env.MONGODB_URI;
+const url = config.MONGODB_URI;
 
-console.log('connecting to', url);
+// console.log('connecting to', url);
 
 mongoose
     .connect(url)
--- fullstackOpen/part3/noteapp/mongo.js
+++ fullstackOpen/part3/noteapp/mongo.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');p
+const mongoose = require('mongoose');
 
-if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
+// if (process.argv.length<3) {
+//     console.log('give password as argument')
+//     process.exit(1)
+// }
 
-const password = encodeURIComponent(process.argv[2]);
-console.log(password)
+// const password = encodeURIComponent(process.argv[2]);
+// console.log(password)
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.2psjv.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+  'mongodb+srv://fullstack:Pikachu18%40@cluster0.2psjv.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.set('strictQuery',false)
 
@@ -22,19 +22,54 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
-//   content: 'Mongose makes thing easy',
-//   important: false,
+//     'content': 'HTML is easy',
+//     'important': true,
 // })
+
+
 
 // note.save().then(result => {
-//   console.log('note saved!', result)
-//   mongoose.connection.close()
+//     console.log('note saved!', result)
+//     mongoose.connection.close()
 // })
 
-Note.find({important: true}).then(result => {
-    result.forEach(note => {
-        console.log(note);
-    });
-
-    mongoose.connection.close();
+const note1 = new Note({
+    'content': 'CSS is easy',
+    'important': true
 })
+
+note1.save().then(result => {
+    console.log('note saved!', result)
+    mongoose.connection.close()
+})
+
+// Note.find({ important: true }).then(result => {
+//     result.forEach(note => {
+//         console.log(note);
+//     });
+
+//     mongoose.connection.close();
+// })
+
+// [
+//     {
+//       "content": "HTML is easy",
+//       "important": true,
+//       "id": "67b0b92115d7a11ce3e9294a"
+//     },
+//     {
+//       "content": "CSS is easy",
+//       "important": true,
+//       "id": "67b0ba785df684732003a99b"
+//     },
+//     {
+//       "content": "Mongose makes thing easy",
+//       "important": false,
+//       "id": "67b0bb392b7151d9fb02b609"
+//     },
+//     {
+//       "content": "postman is good for testing...",
+//       "important": true,
+//       "id": "67bb7778b8f1e50590785ce1"
+//     }
+//   ]
