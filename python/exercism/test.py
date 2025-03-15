90f8db8422dc58f8acc22c3fc01e15ad26a5474f
Prateek <prateekj1211@gmail.com>
2025-03-15 12:38:00 +0530

    Added python code:

---
+++ python/exercism/test.py
@@ -0,0 +1,91 @@
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix and the words with prefix applied, separated
+     by ' :: '.
+
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
+    """
+
+    print()
+    return ' :: '.join([vocab_words[0]] + [vocab_words[0] + word for word in vocab_words[1:]])
+    
+
+
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    :param word: str - of word to remove suffix from.
+    :return: str - of word with suffix removed & spelling adjusted.
+
+    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
+    """
+    if word[-5] == "i"  :
+        return word[:-5] + "y"
+    return word[:-4]
+    # root = word[:-4]
+    # if root[-1] == "i" :
+    #     return root[: -1] + "y"
+    # return root
+
+
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    :param sentence: str - that uses the word in sentence.
+    :param index: int - index of the word to remove and transform.
+    :return: str - word that changes the extracted adjective to a verb.
+
+    For example, ("It got dark as the sun set.", 2) becomes "darken".
+    """
+    # word = sentence.split()[index]
+    return sentence.split()[index] + "en"
+
+
+
+
+
+
+def make_word_groups(vocab_words):
+    """Transform a list containing a prefix and words into a string with the prefix followed by the words with prefix prepended.
+
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
+
+    This function takes a `vocab_words` list and returns a string
+    with the prefix and the words with prefix applied, separated
+     by ' :: '.
+
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
+    """
+    return  f" :: {vocab_words[0]}".join(vocab_words)
+
+
+input_data = ['auto', 'didactic', 'graph', 'mate', 'chrome', 'centric', 'complete',
+              'echolalia', 'encoder', 'biography']
+
+
+def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    :param word: str - of word to remove suffix from.
+    :return: str - of word with suffix removed & spelling adjusted.
+
+    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
+    """
+    x = word.split("ness")[0]
+    if x[-1] == "i":
+        return x[:-1] + "y"
+    return x
+
+
+print(remove_suffix_ness("heaviness"))
