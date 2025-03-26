24cbcbbaac4d0040b0894ada68649ff013dba758
prateek <prateek.jha@yulu.bike>
2025-03-26 17:26:50 +0530

    Added recipe link

--- odin/dom/recipe/script.js
+++ odin/dom/recipe/script.js
@@ -1,11 +1,15 @@
-const mainContainer = document.querySelector('.main');
+const createMenuUI = () => {
+  const mainContainer = document.querySelector('.main');
+  const div = document.createElement('div');
+  const a = document.createElement('a');
+  a.setAttribute('href', './recipes/lasagna.html');
+  a.textContent = 'Lasagna';
+  div.classList.add("block")
+  const heading = document.createElement("h2");
+  heading.textContent = "Menu";
+  div.appendChild(heading);
+  div.appendChild(a);
+  mainContainer.appendChild(div)
+}
 
-const div = document.createElement('div');
-div.setAttribute("id", "menu");
-div.setAttribute("class", "black");
-div.classList.add("block")
-
-const heading = document.createElement("h2");
-heading.textContent = "Menu";
-div.appendChild(heading);
-mainContainer.appendChild(div)
+createMenuUI();
