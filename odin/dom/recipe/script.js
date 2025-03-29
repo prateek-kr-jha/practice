f97afa47b79d04c1cbc566bfb3965b2baca462b9
Prateek <prateekj1211@gmail.com>
2025-03-29 18:28:09 +0530

    Added box shadow

--- odin/dom/recipe/script.js
+++ odin/dom/recipe/script.js
@@ -4,12 +4,12 @@ const createMenuUI = () => {
   const a = document.createElement('a');
   a.setAttribute('href', './recipes/lasagna.html');
   a.textContent = 'Lasagna';
-  div.classList.add("block")
-  const heading = document.createElement("h2");
-  heading.textContent = "Menu";
+  div.classList.add('block');
+  const heading = document.createElement('h2');
+  heading.textContent = 'Menu';
   div.appendChild(heading);
   div.appendChild(a);
-  mainContainer.appendChild(div)
-}
+  mainContainer.appendChild(div);
+};
 
 createMenuUI();
