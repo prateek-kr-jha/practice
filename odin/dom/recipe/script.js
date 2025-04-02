ac721f6b5f3fa44334eb966087416ad97fe82fc0
Prateek <prateekj1211@gmail.com>
2025-04-03 00:48:53 +0530

    Added list

--- odin/dom/recipe/script.js
+++ odin/dom/recipe/script.js
@@ -1,14 +1,25 @@
+const RECIPES = ['Lasagna', 'Ramen', 'Pasta', 'Butter Chicken']
+
 const createMenuUI = () => {
   const mainContainer = document.querySelector('.main');
+
   const div = document.createElement('div');
-  const a = document.createElement('a');
-  a.setAttribute('href', './recipes/lasagna.html');
-  a.textContent = 'Lasagna';
   div.classList.add('block');
   const heading = document.createElement('h2');
   heading.textContent = 'Menu';
   div.appendChild(heading);
-  div.appendChild(a);
+  const menuList = document.createElement('ul');
+  
+  RECIPES.forEach(recipe => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.setAttribute('href', './recipes/lasagna.html');
+    a.textContent = `${recipe}`;
+    li.appendChild(a);
+    menuList.appendChild(li);
+    
+  })
+  div.appendChild(menuList);
   mainContainer.appendChild(div);
 };
 
