25792f4c33a20d71e1e101dc1fb7984fc9a0a72d
Prateek <prateekj1211@gmail.com>
2025-04-05 20:08:43 +0530

    Add resistor color and tolerance lookup tables

---
+++ python/exercism/resistor_color_expert.py
@@ -0,0 +1,23 @@
+VALUE_LOOKUP = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9
+}
+
+TOLERANCE_LOOKUP = {
+    "grey" : 0.05,
+    "violet" : 0.1,
+    "blue" : 0.25,
+    "green" : 0.5,
+    "brown" : 1,
+    "red" : 2,
+    "gold" : 5,
+    "silver" : 10,
+}
