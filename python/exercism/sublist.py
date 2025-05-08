f277e87cc108fc150c20cadc601aa281c47dc914
Prateek <prateekj1211@gmail.com>
2025-05-08 08:11:09 +0530

    Added is_sublist using any and all

--- python/exercism/sublist.py
+++ python/exercism/sublist.py
@@ -3,24 +3,36 @@ SUPERLIST = None
 EQUAL = None
 UNEQUAL = None
 
+# naive
+# def is_sublist(smaller, larger):
+#     m, n = len(smaller), len(larger)
+
+#     if m == 0:
+#         return True
+    
+#     for i in range(n - m + 1):
+#         if larger[i: i + m] == smaller:
+#             return True
+    
+#     return False
+            
+# using any and all(more pythonic)
 def is_sublist(smaller, larger):
     m, n = len(smaller), len(larger)
 
-    if m == 0:
-        return True
-    
-    for i in range(n - m + 1):
-        if larger[i: i + m] == smaller:
-            return True
-    
-    return False
+    return any(
+        all(larger[i + j] == smaller[j] for j in range(m))
+        for i in range(n - m + 1)
+    )
             
-
-
 
 def sublist(list_one, list_two):
     if list_one == list_two:
         EQUAL = 1
+    elif is_sublist(list_one, list_two):
+        SUBLIST = 1
+    elif is_sublist(list_two, list_one):
+        SUPERLIST = 1
     else:
         UNEQUAL = 1
 
