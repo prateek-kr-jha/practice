2757aed71f6302635426540247a42b5bb31d11dc
prateek <prateek.jha@yulu.bike>
2025-05-16 13:15:01 +0530

    added anagram

---
+++ python/exercism/anagram.py
@@ -0,0 +1,10 @@
+def find_anagrams(word, candidates):
+    return [
+        candidate
+        for candidate in candidates
+        if candidate.lower() != word.lower()
+        and sorted(candidate.lower()) == sorted(word.lower())
+    ]
+
+candidates = ["lemons", "cherry", "melons"]
+print(find_anagrams("solemn", candidates))
---
+++ python/exercism/poker.py
@@ -0,0 +1,16 @@
+def best_hands(hands):
+    """
+    1. five of a kind
+    2. straight flush
+    3. four of a kind
+    4. full house = 3 + 2 of a kind
+    5. flush = same color
+    6. straight
+    7. three of a kind
+    8. two pair
+    9. one pair
+    10. high card
+
+
+    """
+    pass
