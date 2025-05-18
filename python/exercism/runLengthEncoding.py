bec6aa8936abc6c65b840d7b2bbca044e704a579
prateek <prateek.jha@yulu.bike>
2025-05-18 12:02:20 +0530

    completed RLE

--- python/exercism/runLengthEncoding.py
+++ python/exercism/runLengthEncoding.py
@@ -1,10 +1,39 @@
 def decode(string):
-    pass
+    decoded = ""
+    i = 0
+
+    while i < len(string):
+        if string[i].isdigit():
+            digit = string[i]
+            while string[i + 1].isdigit():
+                digit += string[i + 1]
+                i += 1
+            
+            decoded += int(digit) * string[i + 1]
+            i += 2
+        else:
+            decoded += string[i]
+            i +=1
+
+    return decoded
 
 
 def encode(string):
-    pass
+    if len(string) == 1:
+        return string
+    curr = string[0]
+    encoded = ""
+    count = 0
+    for character in string:
+        if character == curr:
+            count += 1
+        else:
+            encoded += curr if count == 1 else (f"{count}{curr}")
+            count = 1
+            curr = character
+    encoded += curr if count == 1 else (f"{count}{curr}")
+    return encoded
 
 
-print(encode("AABBBCCDAA"))
-print(decode("2A3B4C2A"))
+print(encode("zzz ZZ  zZ"))
+print(decode("12WB12W3B24WB") == "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")
